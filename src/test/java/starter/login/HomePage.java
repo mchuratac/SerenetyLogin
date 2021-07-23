e4a8291062da03ada3ced21062c21970950a2ab9
Steven Junco <ajuncov.18@gmail.com>
2021-07-23 17:59:54 -0500

    Refactorizando clases avance

--- src/test/java/starter/login/HomePage.java
+++ src/test/java/starter/login/HomePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 
 public class HomePage {
 
-    public static final Target TITLE_WELCOME =  Target.the("title welcome").located(By.xpath("/html/body/table/tbody/tr[1]/td/table[2]/tbody/tr/td[1]/p/span/b"));
+    public static final Target TITLE_WELCOME =  Target.the("title welcome").located(By.xpath("//*[@id=\"dialog1\"]/img"));
     static Target BTN_CERRAR = Target.the("user field")
             .located(By.linkText("CERRAR SESION"));
 
